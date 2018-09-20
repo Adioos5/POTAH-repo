ae0fe13d05bf1877678b91762378835531c7a763
unknown <a_d_i10@wp.pl>
2018-09-20 21:21:54 +0200

    Added a title to the menu

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,11 +1,19 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.embed.swing.JFXPanel;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.net.URL;
 
 public class Main extends Application {
 
@@ -29,8 +37,15 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        JFXPanel panel = new JFXPanel();
+
+        final URL songPath = Main.class.getResource("/music/menu_theme.mp3");
+        Media sound = new Media(songPath.toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(sound);
+        mediaPlayer.play();
+
         launch(args);
-        
+
     }
 
     public Pane createMenu(){
@@ -39,7 +54,7 @@ public class Main extends Application {
         Button play = new Button("New game");
         play.setPrefHeight(50);
         play.setPrefWidth(200);
-        play.setTranslateX(100);
+        play.setTranslateX(1000);
         play.setTranslateY(150);
         play.setOnMouseClicked(event -> {
             createNewGameWindow();
@@ -48,7 +63,7 @@ public class Main extends Application {
         Button load = new Button("Load game");
         load.setPrefHeight(50);
         load.setPrefWidth(200);
-        load.setTranslateX(100);
+        load.setTranslateX(1000);
         load.setTranslateY(250);
         load.setOnMouseClicked(event -> {
             createLoadGameWindow();
@@ -57,7 +72,7 @@ public class Main extends Application {
         Button settings = new Button("Settings");
         settings.setPrefHeight(50);
         settings.setPrefWidth(200);
-        settings.setTranslateX(100);
+        settings.setTranslateX(1000);
         settings.setTranslateY(350);
         settings.setOnMouseClicked(event -> {
             createSettingsWindow();
@@ -66,13 +81,26 @@ public class Main extends Application {
         Button quit = new Button("Quit game");
         quit.setPrefHeight(50);
         quit.setPrefWidth(200);
-        quit.setTranslateX(100);
+        quit.setTranslateX(1000);
         quit.setTranslateY(450);
 
         quit.setOnMouseClicked(event -> {
             System.exit(0);
         });
 
+        Text title = new Text("Path Of The Ancient Hero");
+        title.setFont(Font.font("Arial", 50));
+        title.setX(200);
+        title.setY(130);
+
+        ImageView imgV = new ImageView(new Image("menu_image.png"));
+        imgV.setX(0);
+        imgV.setY(0);
+        imgV.setFitWidth(1300);
+        imgV.setFitHeight(700);
+
+        root.getChildren().add(imgV);
+        root.getChildren().add(title);
         root.getChildren().add(play);
         root.getChildren().add(load);
         root.getChildren().add(settings);
