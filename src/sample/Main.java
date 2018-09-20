efb51aa8b66dbd92620fd28b6d8c337bf921fc20
unknown <a_d_i10@wp.pl>
2018-09-20 20:52:34 +0200

    Removed vulgar content from the code

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -30,7 +30,7 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
-        System.out.println("chuj");
+        
     }
 
     public Pane createMenu(){
